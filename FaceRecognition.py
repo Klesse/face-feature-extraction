266fe935cdcb6b3aed20bb602af266701f3ffcff
Klesse <pedro_klesse@hotmail.com>
2024-01-13 15:43:28 -0300

    feat: starting with the web interface

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -4,7 +4,7 @@ CREATE TABLE face_features(id SERIAL PRIMARY KEY,
                            access TIMESTAMP,
                            feature JSON);
 """
-
+import tensorflow as tf
 from tensorflow import keras
 from keras.applications import VGG16
 from keras.models import Model
@@ -14,6 +14,7 @@ import psycopg2
 import numpy as np
 import json
 import datetime
+
 
 class FaceRecognition:
     def __init__(self):
--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-import flet
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import flet as ft
 from FaceRecognition import *
 
-def main():
-    face = FaceRecognition()
 
-    # End of program
-    face.conn.close()
-    face.cur.close()
+def main(page: ft.Page):
+    # face = FaceRecognition()
+
+    page.title = "Face Recognition"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    
+    t = ft.Text(value='Hello, world!')
+    page.controls.append(t)
+    
+    page.update()
+
 
 if __name__=="__main__":
-    main()
+    ft.app(target=main, view=ft.WEB_BROWSER) # , view=ft.AppView.WEB_BROWSER
+    #flet.app(port=8550, target=main)

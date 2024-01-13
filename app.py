bac34823fce983a3ba82ebd1829e4cb0d0310f49
Klesse <pedro_klesse@hotmail.com>
2024-01-13 16:23:50 -0300

    feat: adding title, text input and button to the app interface

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import flet as ft
 from FaceRecognition import *
+import cv2
 
 
 def main(page: ft.Page):
@@ -11,12 +12,23 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     
-    t = ft.Text(value='Hello, world!')
-    page.controls.append(t)
+    title = ft.Text(value = 'Face Recognition', 
+                    size=22,
+                    width=300,
+                    weight=ft.FontWeight.BOLD,
+                    text_align = ft.TextAlign.CENTER)
+    
+    text_field = ft.TextField(value="Name", width=300, text_align=ft.TextAlign.CENTER)
+    button_add = ft.FilledButton(text="Add", width=300, icon="add")
+    #page.controls.append(title)
+
+    page.add(
+        ft.Column([title, text_field, button_add])
+    )
     
     page.update()
 
 
 if __name__=="__main__":
-    ft.app(target=main, view=ft.WEB_BROWSER) # , view=ft.AppView.WEB_BROWSER
-    #flet.app(port=8550, target=main)
+    ft.app(target=main, view=ft.AppView.WEB_BROWSER) # , view=ft.AppView.WEB_BROWSER
+    #ft.app(port=8550, target=main)
